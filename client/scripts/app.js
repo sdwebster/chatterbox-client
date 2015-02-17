eb7b6873bfec0f3f6edb0aae64915fea1ed45b41
Hack Reactor Students <accounts@hackreactor.com>
2015-02-16 17:43:10 -0800

    Enable sending and posting messages, and construct part of dom tree

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,54 @@
 // YOUR CODE HERE:
 
+var app = {};
+
+app.server = 'https://api.parse.com/1/classes/chatterbox';
+
+app.init = function() {
+  this.fetch();
+};
+
+app.send = function(message) {
+  $.ajax({
+    // always use this url
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: this.server,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(JSON.stringify(data)/*.username.split("")*/);
+      _.each(data.results,function(message){
+        var newMessage = $('<div class="chat"></div>');
+        newMessage.append('<div class="username">' + _.escape(message.username) + '</div>');
+        newMessage.append('<div class="text">' + _.escape(message.text) + '</div>');
+
+        $('#chats').append(newMessage);
+      });
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to retrieve message');
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.init();
