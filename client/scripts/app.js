910ffe862dbe7267bb8442384d3821d08769d465
Hack Reactor Students <accounts@hackreactor.com>
2015-02-16 19:58:55 -0800

    Make a button to clear messages and a form to post a new chat

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,21 @@
 
 var app = {};
 
+$(document).ready(function() {
+  $('#clear').click(app.clearMessages);
+  $('#submitChat').click(function(event){
+    event.preventDefault();
+    var message = {};
+    message.username = window.location.search.substring(window.location.search.indexOf('=')+1)
+"anonymous";
+    message.text = $('#inputChat').text();
+   // message.createdAt =
+    message.roomname = 'lobby';
+    console.log(message);
+    app.send(message);
+  });
+});
+
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 
 app.init = function() {
@@ -9,6 +24,7 @@ app.init = function() {
 };
 
 app.send = function(message) {
+  console.log('enter app.send method');
   $.ajax({
     // always use this url
     url: this.server,
@@ -17,17 +33,19 @@ app.send = function(message) {
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
+      app.fetch();
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message');
     }
   });
+
 };
 
 app.fetch = function() {
   $.ajax({
-    url: this.server,
+    url: this.server + '?order=-createdAt',
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
